3648330a56f22fb559358caf603b6cd9c99ed726
Rahul <wakude2@gmail.com>
2026-09-04 11:43:42 +0530

    Auth Service Completed

--- AUTH/src/main/java/com/auth/util/JwtUtil.java
+++ AUTH/src/main/java/com/auth/util/JwtUtil.java
@@ -1,12 +1,46 @@
 package com.auth.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class JwtUtil {
 
+    public static final String SECRET_KEY = "myVeryStrongSecretKeyForJwtAuthentication123456";
+    public String generateToken(String username){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", "rahul@gmail.com");
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 5))
+                .claims(claims)
+                .signWith(getKey())
+                .compact();
 
-    public String generateToken(){
+    }
+    private Key getKey(){
+       byte[] bytes= Base64.getDecoder().decode(SECRET_KEY);
+        //byte[] bytes1= SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(bytes);
 
+    }
+    private Claims getClaims(String token){
+        return Jwts.parser().verifyWith((SecretKey) getKey())
+                .build().parseSignedClaims(token)
+                .getPayload();
+    }
+    public Date extractExpiration(String token){
+        return getClaims(token).getExpiration();
     }
 }
